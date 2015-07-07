b26b8dbcc75bb7978a23c19e39adef220639caa7
Christopher Baker <me@christopherbaker.net>
2015-07-07 17:12:55 -0500

    Update addon_config.mk.

---
+++ addon_config.mk
@@ -0,0 +1,8 @@
+meta:
+	ADDON_NAME = ofxTwitterTools
+	ADDON_DESCRIPTION = A collection tools for interacting with Twitter.
+	ADDON_AUTHOR = bakercp
+	ADDON_TAGS = "twitter" "http" "streaming" "twitter api"
+	ADDON_URL = http://github.com/bakercp/ofxTwitterTools
+common:
+	ADDON_DEPENDENCIES = ofxHTTP ofxGeo
