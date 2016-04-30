72d0cf7ace154a57b3ad0a6dd54080d3179287e9
Christopher Baker <me@christopherbaker.net>
2016-04-30 13:11:33 -0500

    Update addon_config.mk

--- addon_config.mk
+++ addon_config.mk
@@ -3,6 +3,6 @@ meta:
 	ADDON_DESCRIPTION = Streaming and REST Twitter Clients for openFrameworks.
 	ADDON_AUTHOR = bakercp
 	ADDON_TAGS = "twitter" "http" "streaming" "twitter api"
-	ADDON_URL = http://github.com/bakercp/ofxTwitterTools
+	ADDON_URL = http://github.com/bakercp/ofxTwitter
 common:
 	ADDON_DEPENDENCIES = ofxHTTP ofxGeo
